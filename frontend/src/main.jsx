0a406c81523732c251838e3c29b10794824e616f
jcastillo0220 <jjcastillo8015@gmail.com>
2026-02-16 23:41:47 -0600

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	renamed:    frontend/src/call.js -> frontend/src/api.js 	new file:   frontend/src/main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AIChat from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AIChat />
+  </React.StrictMode>
+);
